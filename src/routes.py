27bebd2a1f2e4b45ed97730000a7537fe0c12669
dustinreed-info <dustin@dustinreed.info>
2019-09-11 16:42:03 -0500

    Added Routes for About, Certifications, Projects, and route for link to Resume.

--- src/routes.py
+++ src/routes.py
@@ -12,3 +12,12 @@ def contact():
 @app.route('/about')
 def about():
     return render_template('about.html')
+@app.route('/certifications')
+def certifications():
+    return render_template('certifications.html')
+@app.route('/resume')
+def resume():
+    return url_for('static', filename='Resume-Reed-Dustin.pdf')
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
