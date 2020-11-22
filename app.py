69017ddfaef227ce2ffadff265f50e5b997bc1da
Dustin Reed <dustin@dustinreed.info>
2020-11-22 13:51:10 -0600

    Updated @application.route for beanstalk

--- app.py
+++ app.py
@@ -5,37 +5,37 @@ from flask import send_file, send_from_directory
 
 
 def create_app(config_filename=None):
-    app = Flask(__name__)
+    application = Flask(__name__)
     if config_filename is not None:
-        app.config.from_pyfile(config_filename)
+        application.config.from_pyfile(config_filename)
 
-    @app.route('/')
-    @app.route('/index')
-    @app.route('/home')
+    @application.route('/')
+    @application.route('/index')
+    @application.route('/home')
     def index():
         return render_template('index.html', title="Home")
 
 
-    @app.route('/contact')
+    @application.route('/contact')
     def contact():
         return render_template('contact.html', title="Contact")
 
 
-    @app.route('/about')
+    @application.route('/about')
     def about():
         return render_template('about.html', title="About")
 
 
-    @app.route('/certs')
-    @app.route('/certifications')
+    @application.route('/certs')
+    @application.route('/certifications')
     def certifications():
         return render_template('certifications.html', title="Certifications")
 
 
-    @app.route('/Resume')
-    @app.route('/Resume-Reed-Dustin')
-    @app.route('/Resume-Reed-Dustin.pdf')
-    @app.route('/resume')
+    @application.route('/Resume')
+    @application.route('/Resume-Reed-Dustin')
+    @application.route('/Resume-Reed-Dustin.pdf')
+    @application.route('/resume')
     def resume():
         return send_file(
             './static/Resume-Reed-Dustin.pdf',
@@ -43,8 +43,8 @@ def create_app(config_filename=None):
         )
 
 
-    @app.route('/aws-cda')
-    @app.route('/aws-cda-cert.pdf')
+    @application.route('/aws-cda')
+    @application.route('/aws-cda-cert.pdf')
     def aws_cda_cert():
         return send_file(
             './static/aws-cda-cert.pdf',
@@ -52,10 +52,10 @@ def create_app(config_filename=None):
         )
 
 
-    @app.route('/aws-csa')
+    @application.route('/aws-csa')
     # Had  typo in old resume.
-    @app.route('/aws-csa.cert.pdf')
-    @app.route('/aws-csa-cert.pdf')
+    @application.route('/aws-csa.cert.pdf')
+    @application.route('/aws-csa-cert.pdf')
     def aws_csa_cert():
         return send_file(
             './static/aws-csa-cert.pdf',
@@ -63,26 +63,28 @@ def create_app(config_filename=None):
         )
 
 
-    @app.route('/projects')
+    @application.route('/projects')
     def projects():
         return render_template('projects.html', title="Projects")
 
 
-    @app.route('/favicon.ico')
+    @application.route('/favicon.ico')
     def favicon():
         return send_from_directory(
-            os.path.join(app.root_path, 'static'),
+            os.path.join(application.root_path, 'static'),
             'favicon.ico',
             mimetype='image/vnd.microsoft.icon'
         )
 
 
-    @app.errorhandler(404)
+    @application.errorhandler(404)
     def page_not_found(e):
         return render_template('404.html', title="Error"), 404
 
-    return app
+    return application
 
+
+application = create_app()
 
 if __name__ == "__main__":
-    create_app.run("0.0.0.0")
+    application.run("0.0.0.0")
